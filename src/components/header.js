f5016611bda5359b8f891be1da3cd052a4b36585
Areh Tunde <arehtunde96@gmail.com>
2021-01-07 22:27:15 +0100

    add icon to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 const Header = () => (
   <header className='flex bg-yellow-400 bg-opacity-75 font-bold text-3xl px-4 py-2'>
-    <svg data-src="https://s.svgbox.net/materialui.svg?ic=note" width="32" height="32" fill="currentColor"></svg>
+    <svg data-src="https://s.svgbox.net/materialui.svg?ic=note" width="32" height="32" fill="currentColor" className='transform -rotate-90'></svg>
     <h1 className='ml-2'>Notes</h1>
   </header>
 );
